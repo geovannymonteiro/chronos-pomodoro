7d35d295521b5162bbc7f1d40c12835a94d420c1
Geovanny Monteiro <geovannymonteiro@outlook.com>
2026-01-05 19:36:16 -0400

    adicionei a logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import { Heading } from './components/Heading';
-
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
 import './styles/theme.css';
 import './styles/global.css';
 
 export function App() {
-  console.log('Oi');
   return (
-    <div>
-      <Heading />
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique voluptatem, magnam impedit ratione
-        repudiandae repellendus, praesentium rerum necessitatibus, iure vitae veniam libero ea non minus explicabo Vel
-        ipsa earum accusantium.
-      </p>
-    </div>
+    <>
+      <Container>
+        <Logo />
+      </Container>
+
+      <Container>
+        <Heading>MENU</Heading>
+      </Container>
+    </>
   );
 }
 export default App;
