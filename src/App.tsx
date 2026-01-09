cd885319bdb49757cfb2f642d11525cdfe111079
Geovanny Monteiro <geovannymonteiro@outlook.com>
2026-01-09 14:46:19 -0400

    adicionei o menu eo timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import { Heading } from './components/Heading';
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import './styles/theme.css';
 import './styles/global.css';
+import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDrown';
+import { DefaultInput } from './components/Defaultinput';
 
 export function App() {
   return (
@@ -12,7 +14,29 @@ export function App() {
       </Container>
 
       <Container>
-        <Heading>MENU</Heading>
+        <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
+      </Container>
+
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <DefaultInput id='meuInput' type='' />
+          </div>
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className='formRow'>
+            <p> Ciclos</p>
+            <p>0 0 0 0</p>
+          </div>
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
