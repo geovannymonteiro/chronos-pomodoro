3b0762e45e32846cefcc1e4a77bdfa074b5ac0c7
Geovanny Monteiro <geovannymonteiro@outlook.com>
2026-03-27 22:10:21 -0400

    capturando evento de clique

---
+++ src/components/CountDrown/index.tsx
@@ -0,0 +1,5 @@
+import styles from './styeles.module.css';
+
+export function CountDown() {
+  return <div className={styles.container}>00:00</div>;
+}
---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,35 @@
+import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
+import styles from './styeles.module.css';
+import { useState } from 'react';
+
+type AvailableThemes = 'dark' | 'light';
+
+export function Menu() {
+  const [theme, setTheme] = useState<AvailableThemes>('dark');
+
+  function handleThemeChange(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+    event.preventDefault(); // Não segue o link
+    console.log('Clicado', Date.now());
+  }
+
+  return (
+    <nav className={styles.menu}>
+      <h1>{theme}</h1>
+      <a className={styles.menuLink} href='#' aria-label='Ir para a Home' title='Ir para a Home'>
+        <HouseIcon />
+      </a>
+
+      <a className={styles.menuLink} href='#' aria-label='Ver Histórico' title='Ver Histórico'>
+        <HistoryIcon />
+      </a>
+
+      <a className={styles.menuLink} href='#' aria-label='Ir para as Configurações' title='Ir para as Configurações'>
+        <SettingsIcon />
+      </a>
+      <a className={styles.menuLink} href='#' aria-label='Mudar Tema' title='Mudar Tema'
+      onClick={handleThemeChange}>
+        <SunIcon />
+      </a>
+    </nav>
+  );
+}
